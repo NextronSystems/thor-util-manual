a5f73216d89b6947ee7931dcda6363f8187273ab
Florian Roth <venom14@gmail.com>
2021-05-04 11:02:00 +0200

    docs: relative links

--- conf.py
+++ conf.py
@@ -180,3 +180,6 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# Enable -- to -
+smartquotes = False
